1ccdd730940a02e25471b6a841477e513e5cb3f0
james-pcdf <107222062+james-pcdf@users.noreply.github.com>
2022-07-19 13:04:05 -0400

    work on zmq basic

--- topics/gnu_python_integration/zmq_basic_1/zmq_push_basic_2_PURE.py
+++ topics/gnu_python_integration/zmq_basic_1/zmq_push_basic_2_PURE.py
@@ -13,7 +13,19 @@ def dostuff(tb):
     push_sock = makeZMQPushSocket("tcp://127.0.0.1:50252")
     
     ones = np.ones(100000, dtype=np.complex64)
-    push_sock.send(ones.tobytes())
+    zeros = np.zeros(100000, dtype=np.complex64)
+
+    count = 0
+    while True:
+        
+        for unused in range(500):
+            push_sock.send(ones.tobytes())
+
+        for unused in range(10):
+            push_sock.send(zeros.tobytes())
+
+        count += 1
+        print("buffered:", count)
     
 
 set_up_qt_top_block_and_run_func_in_thread(zmq_pull_GRCGEN, dostuff)
