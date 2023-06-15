7e941f619586e1037c9ef065acb6d3f155d0c5a7
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-06-15 13:36:56 -0400

    Create rolling_code.py

---
+++ classroom_activities/Chx_Misc/Unfinished/rolling_code.py
@@ -0,0 +1,12 @@
+
+code = 273
+print(code)
+code = (code * 384) % 1000
+print(code)
+code = (code * 384) % 1000
+print(code)
+code = (code * 384) % 1000
+print(code)
+
+# print(12345 % 1000)
+# print(6237 % 1000)
