434cd9c855241dce4b5682f0c7c7d889b6563a3e
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-07-07 13:44:37 -0400

    Create noisify.py

---
+++ classroom_activities/Chx_Misc/Python_interactions/noisify.py
@@ -0,0 +1,20 @@
+"""
+Reads the specified file,
+Adds random noise,
+scales down (to avoid amplitude being unrealistically large)
+writes result to a new file.
+"""
+import numpy as np
+
+infilename = "unknown_signal_1.complex"
+outfilename = "generated_noisy_signal.complex"
+scale_factor = 0.2
+
+dat = np.fromfile(infilename, dtype=np.complex64)
+
+noise_real = np.random.normal(0, .5, len(dat))
+noise_imag = np.random.normal(0, .5, len(dat))
+
+noisydat = scale_factor * np.complex64(dat + noise_real + 1j*noise_imag)
+
+noisydat.tofile(outfilename)
