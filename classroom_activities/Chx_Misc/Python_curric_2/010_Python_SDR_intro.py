60904466b146997df7f236e4a9bdf6117034d45c
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-07-17 00:21:06 -0400

    Create 010_Python_SDR_intro.py

---
+++ classroom_activities/Chx_Misc/Python_curric_2/010_Python_SDR_intro.py
@@ -0,0 +1,5 @@
+try:
+    import pcdr
+    print("Imported pcdr module successfully.")
+except ImportError:
+    print("Please download the pcdr.py file from https://raw.githubusercontent.com/python-can-define-radio/sdr-course/main/classroom_activities/Chx_Misc/Python_curric_2/pcdr.py")
