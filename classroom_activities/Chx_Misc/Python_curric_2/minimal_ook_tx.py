7a291204189a0224e065565687a45aa3b8cbdaf9
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-07-22 00:09:17 -0400

    Update minimal_ook_tx.py

--- classroom_activities/Chx_Misc/Python_curric_2/minimal_ook_tx.py
+++ classroom_activities/Chx_Misc/Python_curric_2/minimal_ook_tx.py
@@ -11,6 +11,7 @@ sender.send([1, 0, 1, 0, 1, 0, 0, 1])
 ## (This part is not for the students)
 ## Behind the scenes, .send() will...
 ##   if any are not in [0, 1]: raise ValueError 
+##   convert bit_length to an int
 ##   repeated_bits = pydash flatmap: repeat each item bit_length times 
 ##   padded_rb = pad repeated_bits with zeros to nearest multiple of 1024
 ##   chunked = pydash.chunk padded_rb, size=1024
@@ -63,11 +64,73 @@ sender.send([1, 0, 1, 0, 1, 0, 0, 1])
 sender = stg.osmocom_ook_sender(samp_rate=2e6, bit_length=1e6)
 sender.send([0, 1, 0, 0, 0, 0, 1, 1])
 
+
 ## 6
 ## If you tried sending the letter C, you may have noticed that since we're
 ## using On-Off Keying (OOK), it's not obvious where the transmission starts.
 ## Because of that, URH may have trouble converting this to ASCII.
 ## So, let's begin our transmission with a preamble.
 sender = stg.osmocom_ook_sender(samp_rate=2e6, bit_length=1e6)
-sender.send([1, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 1])
+sender.send([1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1])
+
+
+## 7a
+## Writing the binary explicitly is getting somewhat tedious.
+## Here's a function that converts a string to binary.
+## Interested readers may wish to look up "list comprehensions 
+##   in Python", but we can use this function without fully 
+##   understanding the implementation details.
+def str_to_bin_list(mystr):
+     bitstrs = [f"{x:08b}" for x in mystr.encode("utf")]
+     joined = "".join(bitstrs)
+     return list(map(int, joined))
+
+
+## 7b
+## Let's try it. This should print the same list we saw earlier,
+## specifically, [0, 1, 0, 0, 0, 0, 1, 1].
+list_of_bits = str_to_bin_list("C")
+print(list_of_bits)
+
+
+## 8
+## Try this. Verify that it prints the expected sequence of 16 bits.
+list_of_bits = str_to_bin_list("Hi")
+print(list_of_bits)
+
+
+## 9
+## Now that we can use str_to_bin_list, it's much easier to send messages.
+## Try this:
+list_of_bits = str_to_bin_list("Hi")
+sender = stg.osmocom_ook_sender(samp_rate=2e6, bit_length=1e6)
+sender.send(list_of_bits)
+
+
+## 10
+## You may have noticed an issue: we've lost our preamble.
+## We're going to use the same symbol we used before, "«", whose binary
+## representation (10101011) has two characteristics that we want:
+##  - it starts with a 1
+##  - it alternates between 1 and 0 often, which makes it recognizable
+## Let's send a message with that preamble, and interpret it with URH.
+list_of_bits = str_to_bin_list("«Hi")
+sender = stg.osmocom_ook_sender(samp_rate=2e6, bit_length=1e6)
+sender.send(list_of_bits)
+
+
+## 11
+## Copy and modify the previous example to use input() to 
+## ask the user for a string to send.
+## Once the user has provided a string, prepend our preamble 
+## before sending the message.
+
+
+## 12
+## Copy and modify the previous example to ask the user for
+##  - a string to send
+##  - the bit length
+## As before, include a preamble.
+
+
 
