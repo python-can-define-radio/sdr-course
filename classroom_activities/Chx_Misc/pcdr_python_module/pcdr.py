d2bdf993a4c4c109fe48e91573b0d019fddaf0ef
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-07-29 21:28:29 -0400

    setup.py approach to packaging

---
+++ classroom_activities/Chx_Misc/pcdr_python_module/pcdr.py
@@ -0,0 +1,2 @@
+def addone(x):
+    return x + 1
---
+++ classroom_activities/Chx_Misc/pcdr_python_module/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
+
+
+# [project]
+# name = "pcdr"
+# version = "0.0.1"
+# description = "python-can-define-radio SDR class functionality."
+# requires-python = ">=3.7"
+# classifiers = [
+#     "Programming Language :: Python :: 3",
+#     "Operating System :: OS Independent",
+# ]
+
+# [project.urls]
+# "Homepage" = "https://github.com/python-can-define-radio/sdr-course"
+# "Bug Tracker" = "https://github.com/python-can-define-radio/sdr-course/issues"
