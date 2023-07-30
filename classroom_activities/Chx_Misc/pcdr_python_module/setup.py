9affbc2d289ed09bb890ec5f739917e074162dbf
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-07-29 22:01:36 -0400

    remove unneeded parts

--- classroom_activities/Chx_Misc/pcdr_python_module/setup.py
+++ classroom_activities/Chx_Misc/pcdr_python_module/setup.py
@@ -9,13 +9,13 @@ https://github.com/pypa/sampleproject
 """
 
 # Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-import pathlib
+from setuptools import setup  # , find_packages
+# import pathlib
 
-here = pathlib.Path(__file__).parent.resolve()
+# here = pathlib.Path(__file__).parent.resolve()
 
 # Get the long description from the README file
-long_description = (here / "README.md").read_text(encoding="utf-8")
+# long_description = (here / "README.md").read_text(encoding="utf-8")
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
