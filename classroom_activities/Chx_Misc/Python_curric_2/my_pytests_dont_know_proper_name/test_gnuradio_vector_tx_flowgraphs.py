7e08a2660adced5e6f87214f518f78e2d3c3fd21
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-09-07 16:05:24 -0400

    add test

---
+++ classroom_activities/Chx_Misc/Python_curric_2/my_pytests_dont_know_proper_name/test_gnuradio_vector_tx_flowgraphs.py
@@ -0,0 +1,24 @@
+from pcdr import gnuradio_write_file, makeComplexWave_time
+import numpy as np
+import random
+from pathlib import Path
+
+
+def test_write_and_read():
+    basepath = Path("temp_dir_for_tests")
+    fn = str(basepath / "samprate_2e6_basic_wave.complex")
+
+    len_dat = random.randint(200, 30000)
+    dat_to_write = np.complex64(np.random.random(len_dat))
+    # timestamps, wave = makeComplexWave_time(2, 1000, 10)
+    assert len(dat_to_write) == len_dat
+    assert isinstance(dat_to_write, np.ndarray)
+    gnuradio_write_file(dat_to_write, fn)
+    
+    nploaded = np.fromfile(fn, np.complex64)
+    assert len(nploaded) <= len(dat_to_write)
+    subset_of_original_that_actually_got_written = dat_to_write[:len(nploaded)]
+    assert (nploaded == subset_of_original_that_actually_got_written).all()
+    
+
+
