225cf6482ce89b85077aebaa710baf46ff8c671a
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2023-09-21 15:10:55 -0400

    Create car_using_pcdr.py

---
+++ classroom_activities/Chx_Misc/Python_interactions/car_using_pcdr.py
@@ -0,0 +1,9 @@
+from pcdr import gnuradio_send, ook_modulate
+import numpy as np
+
+buncha_1_0 = [1,0] * 35
+fourLong = [1,1,1,0] * 4
+
+carsig_bits = fourLong + buncha_1_0
+carsig = ook_modulate(carsig_bits, bit_length=1055)
+gnuradio_send(carsig, center_freq=49.86e6, samp_rate=2e6, if_gain=35, repeat=True)
