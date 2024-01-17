e58e8101382910973a0cb753767a5b3250d03b5b
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-01-17 14:31:43 -0500

    pcdr: Update simple.py: device name and id

--- classroom_activities/Chx_Misc/Python_curric_2/pcdr/simple.py
+++ classroom_activities/Chx_Misc/Python_curric_2/pcdr/simple.py
@@ -18,12 +18,19 @@ class OsmosdrReceiver:
     print(strength)
     ```
     """
-    def __init__(self, center_freq: float):
+    def __init__(self, device_name: str, device_id: str, center_freq: float):
+        """
+        `device_name`: One of the supported osmocom devices, such as hackrf, bladerf, etc (see the osmocom docs)
+        `device_id`: A zero-based index ("0", "1", etc), or the partial serial number of the device, which can be gotten from GQRX
+
+        >>> 
+        """
         self.tb = gr.top_block()
         self.freq_offset = 20e3
         self.samp_rate = 2e6
         self.fft_size = 1024
-        self.osmo_source = osmo_source(args="hackrf=0")
+        device_args = f"{device_name}={device_id}"
+        self.osmo_source = osmo_source(args=device_args)
         self.osmo_source.set_sample_rate(self.samp_rate)
         self.osmo_source.set_center_freq(center_freq - self.freq_offset)
         self.osmo_source.set_if_gain(32)
@@ -50,7 +57,20 @@ class OsmosdrReceiver:
     
 
     def set_sample_rate(self, samp_rate: float):
+        # TODO: Validate samp rate within range
         return self.osmo_source.set_sample_rate(samp_rate)
 
     def set_center_freq(self, center_freq: float):
+        # TODO: Validate freq within range
+        # Also, TODO:
+        #   Tell the queue work function to consume the entire input_items so that
+        #   any data after that is fresh, THEN clear the current get_cf_strength() reading (which
+        #   will presumably be using a deque object).
         return self.osmo_source.set_center_freq(center_freq - self.freq_offset)
+
+    def set_if_gain(self, TODO):
+        TODO
+
+    def set_bb_gain(self, TODO):
+        TODO
+        
