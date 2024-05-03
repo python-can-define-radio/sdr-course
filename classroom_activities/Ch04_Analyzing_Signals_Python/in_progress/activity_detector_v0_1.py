5ce65b7b913fb2b007a0fbf95f0e21d1b853b4dc
james-pcdr <107222062+james-pcdr@users.noreply.github.com>
2024-05-03 10:57:50 -0400

    Create activity_detector_v0_1.py

---
+++ classroom_activities/Ch04_Analyzing_Signals_Python/in_progress/activity_detector_v0_1.py
@@ -0,0 +1,51 @@
+import datetime
+import time
+from pcdr.flow import OsmoSingleFreqReceiver
+import os
+from playsound import playsound
+ 
+ 
+def safePlay(fn, block):
+    try:
+        playsound(fn, block=block)
+    except Exception:
+        print("Failed to play the sound.")
+ 
+ 
+rec = OsmoSingleFreqReceiver("hackrf=0", 462.7e6)
+rec.start()
+short_avg = 0
+long_avg = 0
+count = 0
+fn = "activity.csv"
+if os.path.exists(fn):
+    newfile = False
+else:
+    newfile = True
+with open(fn, "a", encoding="utf-8") as f:
+    if newfile:
+        f.write("passedThresh,date,time,unix_timestamp,strength,short_avg,long_avg\n")
+    while True:
+        stren = rec.get_strength()
+        short_avg = 0.9*short_avg + 0.1*stren
+        long_avg = 0.9999*long_avg + 0.0001*stren
+        count += 1
+        if count == 100:  # Record every hundredth
+            count = 0
+ 
+            nowtime = time.time()
+            dt = datetime.datetime.fromtimestamp(nowtime)
+            print(f"{dt}  Recording to file.")
+ 
+            if short_avg > 1.2 * long_avg:
+                safePlay("Bell.wav", block=False)
+                print("BIG SPIKE")
+                f.write("BIG SPIKE,")
+            elif short_avg > 1.1 * long_avg:
+                safePlay("Buzzer.wav", block=False)
+                print("SPIKE")
+                f.write("SPIKE,")
+            else:
+                f.write("NO,")
+            f.write(f"{dt.date()},{dt.time()},{nowtime},{stren},{short_avg},{long_avg}\n")
+            f.flush()
